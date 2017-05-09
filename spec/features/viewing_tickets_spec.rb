cc66aa8b7e784d5ab6a57ad11c1430f266dbc0b8
Maxwell Frederickson <maxwellfrederickson@gmail.com>
2017-05-08 22:24:20 -0500

    fixed tests.  link tickets and users upon creation

--- spec/features/viewing_tickets_spec.rb
+++ spec/features/viewing_tickets_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.feature 'Users can view tickets' do
   before do
+    author = FactoryGirl.create(:user)
+
     sublime = FactoryGirl.create(:project, name: 'Sublime Text 3')
     FactoryGirl.create(
       :ticket,
       project: sublime,
+      author: author,
       name: 'Make it shiny!',
       description: 'Gradients! Starbursts! Oh my!'
     )
@@ -14,6 +17,7 @@ RSpec.feature 'Users can view tickets' do
     FactoryGirl.create(
       :ticket,
       project: ie,
+      author: author,
       name: 'Standards Compliance',
       description: "Isn't a joke."
     )
